47db8b3a6cd1420aa3edcf4c949fe7a16771b5ab
Joe Hudson <joehud@gmail.com>
2014-06-16 21:45:30 -0400

    add bower support

--- README.md
+++ README.md
@@ -8,9 +8,15 @@ Usage
 If you include a lib directory within any *section*, all files within that directly will be included as global resources in the transpiled section javascript code
 ```
     {project root}
-    |-- {section name}
+    |-- js
         |-- lib
-            |-- myfile.js
+            file1.js
+            file1.min.js
+            ...
+    |-- {section name}
+        |-- js
+            |-- lib
+                ...
 ```
 
 Install
@@ -29,6 +35,29 @@ Add this plugin to the gulp-web-module reference in your gulpfile
 
 Options
 -------
-The options that can be provided when creating this plugin is a hash with the following values
-* *dir*: (default "lib") name of the directory containing the javascript files
-* *priority* optional array of file names if any particular order is required - not all file names need to be included and "*" can be used to indicate wildcards
+Not all files in the lib dirs will be included, only those that are referenced in the plugin options.  The options are split out by section and can optionally be specific to a build type.
+```
+    gwmLib({
+      // the base section
+      base: [
+        file1.js (or file-*.js as * is wildcard)
+        {dev: 'file1.js', prod: 'file1.min.js'},
+      ],
+      // another section called "foo"
+      foo: [
+        ...
+      ]
+    })
+```
+
+Bower
+-----
+This plugin understands bower.  All you have to do is to prefix the lib entries with "bower:"" to use bower for the resources.  This is an example of using jquery.
+```
+    gwmLib({
+      // the base section
+      base: [
+        {dev: 'bower:jquery', prod: 'bower:jquery/dist/jquery.min.js'},
+      ]
+    })
+```
--- index.js
+++ index.js
@@ -2,20 +2,75 @@ var fileHeader = require('./lib/file-header'),
     fs = require('fs');
 
 module.exports = function(options) {
-  options = options || {};
   var dir = options.dir || 'js/lib',
-      priority = options.priority;
+  options = options || {};
 
   return {
     javascript: {
       complete: function(_options, pipeline) {
         var dirPath = _options.srcPath + dir;
-        return pipeline.pipe(fileHeader(dirPath, options, _options.gulp));
+        return pipeline.pipe(fileHeader(dirPath, getOrderedList(_options), options, _options.gulp));
       },
       watch: function(options) {
-        return options.srcPath + dir + '/**/*'
+        if (options.isBase) {
+          var bowerPriority = [];
+
+        }
+        return options.srcPath + dir + '/**/*';
       }
     }
+  };
+
+  function getOrderedList(buildOptions) {
+    var section = buildOptions.section,
+        paths = options[section] || [],
+        bowerComponents = [],
+        buildType = buildOptions.buildType,
+        match, item;
+
+    paths = paths.map(function(path) {
+      if (typeof path === 'string') {
+        return path;
+      } else {
+        return path[buildType] || path.dev;
+      }
+    });
+
+    for (var i=0; i<paths.length; i++) {
+      item = paths[i];
+      match = item.match(/bower:(.*)/);
+      if (match) {
+        bowerComponents.push(match[1]);
+      }
+    }
+
+    // at least for now, synchronously load bower components
+    var indexedBowerComponents = {};
+    for (i=0; i<bowerComponents.length; i++) {
+      indexedBowerComponents['bower:' + bowerComponents[i]] = getBower(bowerComponents[i]);
+    }
+
+    return {
+      list: paths,
+      bower: indexedBowerComponents
+    };
+  }
+
+  function getBower(bowerPath) {
+    var match = bowerPath.match(/([^/]+)\/?(.*)/),
+        bowerKey = match[1],
+        path = match[2],
+        prefix = './bower_components/' + bowerKey + '/';
+    if (!path) {
+      try {
+        path = JSON.parse(fs.readFileSync(prefix + 'bower.json', {encoding: 'utf8'})).main;
+      } catch (e) {}
+    }
+    if (!path) {
+      throw new Error('Unknown bower path: ' + bowerKey);
+    }
+    path = './bower_components/' + bowerKey + '/' + path;
+    var content = fs.readFileSync(path, {encoding: 'utf8'});
+    return content;
   }
 };
-
